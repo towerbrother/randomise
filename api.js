a96ff7106ad9053298b18a721c4a42117e7da594
GTO <giorgio.torre@stellenanzeigen.de>
2022-02-04 07:00:19 +0100

    wrote api data

---
+++ api.js
@@ -0,0 +1,25 @@
+export const participants = [
+    'P1',  'P2',  'P3',  'P4',  'P5',  'P6',
+    'P7',  'P8',  'P9',  'P10', 'P11', 'P12',
+    'P13', 'P14', 'P15', 'P16', 'P17', 'P18',
+    'P19', 'P20', 'P21', 'P22', 'P23', 'P24',
+    'P25', 'P26', 'P27', 'P28', 'P29', 'P30',
+    'P31', 'P32', 'P33', 'P34', 'P35', 'P36',
+    'P37', 'P38', 'P39', 'P40', 'P41', 'P42',
+    'P43', 'P44', 'P45', 'P46', 'P47', 'P48'
+  ];
+
+  export const cognTests = [
+      'stroop',
+      'mapit',
+      'otmt-a',
+      'otmt-b'
+  ];
+
+  export const movTests = [
+      'transition',
+      'iptoap',
+      'aptoip',
+      'dt-otmt-a',
+      'dt-otmt-b',
+  ];
